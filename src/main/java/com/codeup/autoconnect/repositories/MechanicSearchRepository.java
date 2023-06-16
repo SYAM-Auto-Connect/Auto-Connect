f2bd3e18b294df5b6a183aed5a9a56ce77fd3d2d
Amir Saunders <115315294+amir97353@users.noreply.github.com>
2023-06-16 13:58:52 -0500

    search functionality and view all mechanic complete

--- src/main/java/com/codeup/autoconnect/repositories/MechanicSearchRepository.java
+++ src/main/java/com/codeup/autoconnect/repositories/MechanicSearchRepository.java
@@ -4,6 +4,7 @@ import com.codeup.autoconnect.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface MechanicSearchRepository extends JpaRepository<User,Long> {
@@ -14,3 +15,7 @@ public interface MechanicSearchRepository extends JpaRepository<User,Long> {
 
 
 }
+
+
+
+
