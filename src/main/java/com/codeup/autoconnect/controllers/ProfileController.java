1d753b3da5d783ea44078589dea83a99c53657b4
Amir Saunders <115315294+amir97353@users.noreply.github.com>
2023-06-27 16:01:42 -0500

    picture progress

--- src/main/java/com/codeup/autoconnect/controllers/ProfileController.java
+++ src/main/java/com/codeup/autoconnect/controllers/ProfileController.java
@@ -73,11 +73,13 @@ public class ProfileController {
     @PostMapping("/profile/{id}/edit")
     public String submitEditProfile(HttpSession session, @PathVariable long id, @ModelAttribute User editProfile) throws AccessDeniedException {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
         if(user.getId() != id){
             throw new AccessDeniedException("You cannot edit other users' profile");
         }
         userDao.save(editProfile);
         session.invalidate();
+        //Above delete
         return "redirect:/profile";
     }
 
