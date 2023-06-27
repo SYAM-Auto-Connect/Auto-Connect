18c51379cd2679aedafaa391968e16d85b8e1b53
Yong Choi <yong.s.choi2@gmail.com>
2023-06-27 09:25:20 -0500

    useless

--- src/main/java/com/codeup/autoconnect/controllers/HomeController.java
+++ src/main/java/com/codeup/autoconnect/controllers/HomeController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String home(HttpSession session) {
-        session.invalidate();
+    public String home() {
+
         return "home";
     }
 }
